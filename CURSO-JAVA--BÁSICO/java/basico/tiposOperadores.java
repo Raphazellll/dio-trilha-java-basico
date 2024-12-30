421cef3b296f25b8195a0198e5ce2e38961641a7
Raphazelll <pimentaraphael000@gmail.com>
2024-12-30 19:30:52 -0300

    aprendendo a usar um if compactado.

--- CURSO-JAVA--BÁSICO/java/basico/tiposOperadores.java
+++ CURSO-JAVA--BÁSICO/java/basico/tiposOperadores.java
@@ -23,25 +23,50 @@ public static void main(String[] args) {
 
     // ===================================================================================
 
-    //Operador Unários
+   /*  //Operador Unários
 
-    //Esses operadores são aplicados juntamente com um outro operador aritmético.
-    //Eles realizam alguns trabalhos básicos como incrementar, decrementar, inverter valores numéricos e booleanos.
+    Esses operadores são aplicados juntamente com um outro operador aritmético.
+    Eles realizam alguns trabalhos básicos como incrementar, decrementar, inverter valores numéricos e booleanos.
 
-    // (+) Operador unário de valor positivo - números são positivos sem esse operador explicitamente;
-    // (-) Operador unário de valor negativo - nega um número ou expressão aritmética;
-    // (++) Operador unário de incremento de valor - incrementa o valor em 1 unidade;
-    // (--) Operador unário de decremento de valor - decrementa o valor em 1 unidade;
-    // (!) Operador unário lógico de negação - nega o valor de uma expressão booleana.
-    // Exemplos abaixo:
+     (+) Operador unário de valor positivo - números são positivos sem esse operador explicitamente;
+     (-) Operador unário de valor negativo - nega um número ou expressão aritmética;
+     (++) Operador unário de incremento de valor - incrementa o valor em 1 unidade;
+     (--) Operador unário de decremento de valor - decrementa o valor em 1 unidade;
+     (!) Operador unário lógico de negação - nega o valor de uma expressão booleana.
+     Exemplos abaixo: */
+
+    int numero = 5;
+    numero--;
+
+        System.out.println(numero);
+
+    boolean variavel = true;
+    variavel = !variavel; //invertendo uma booleana
+
+        System.out.println(variavel);
 
     // ===================================================================================
 
-    int numeros = 5;
+    /*Ternário
+     O operador de condição Ternária é uma forma resumida para definir uma condição e escolher por
+     um dentre dois valores. Você deve pensar numa condição ternária como se fosse uma condição IF
+     normal, porém, de uma forma que toda a sua estrutura estará escrita numa unica linha.
+      
+     O operador ternário é representado pelos simbolos "?:" utilizados na seguinte estrutura de 
+     seintaxe:
 
-    numeros = numeros - 3;
-    System.out.println(numeros);
+     <expressão condicional>  
+     <Caso condição seja true> = ?
+     <Caso condição seja false> = :
+     */
+int a, b;
+    a = 6;
+    b = 6;
+    String resultado = a==b ?"verdadeiro" : "falso"; 
+//uma outra maneira de usar o if, só que compactado.
+    System.out.println(resultado);
     
+
     }
     
 }
