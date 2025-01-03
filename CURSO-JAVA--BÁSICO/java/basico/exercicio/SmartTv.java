830a8f48c49d5e8349743cc488179fa8a21e5618
Raphazelll <pimentaraphael000@gmail.com>
2025-01-03 16:29:10 -0300

    FIXED: atualizando commit, pequeno problema resolvido no código.

--- CURSO-JAVA--BÁSICO/java/basico/exercicio/SmartTv.java
+++ CURSO-JAVA--BÁSICO/java/basico/exercicio/SmartTv.java
@@ -1,13 +1,14 @@
 package exercicio;
 
 public class SmartTv {
-    /*Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+      /*Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
         1. Ela tenha as características: Ligar (boolean), canal (int) e volume (int).
         2. Nossa TV podedrá ligar e desligar e assim mudar o estado ligada.
         3. Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1.
         4. Nossa TV podedrá mudar de canal de 1 em 1 ou definindo o número correspondente.
      */
-    boolean = ligada = false;
+  
+    boolean ligada = false;
     int canal = 1;
     int volume = 15;
         
